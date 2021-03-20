a66a2793f44ffc9a1beb28bd5cfc7b8e93190d31
Mohan Dere <mohan.dere@thoughtworks.com>
2021-03-20 18:27:44 +0530

    font changes - added open sans

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,15 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-    <link
-      rel="preload"
-      href="/fonts/Inter-roman.latin.var.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
+      <link
+        rel="preload"
+        href="/fonts/Inter-roman.latin.var.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
       />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />
     </>
