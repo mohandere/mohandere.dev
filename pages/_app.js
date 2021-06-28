38b0cd856daaff3fcf52a65f421c9dcd9327d7eb
Mohan Dere <Mohan.Dere@bp.com>
2021-06-28 20:30:11 +0530

    Add like dislike

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,69 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { Provider, UpdownButton } from "@lyket/react";
+import { useRouter } from "next/router";
 
-import 'nextra-theme-blog/style.css'
-import '../styles/main.css'
+import "nextra-theme-blog/style.css";
+import "../styles/main.css";
+
+const canShowUpdownButton = (pathname = "") => {
+  if (pathname.indexOf("/posts/") !== -1) {
+    return true;
+  }
+  if (pathname.indexOf("/dev-tips/") !== -1) {
+    return true;
+  }
+  return false;
+};
+const getShowUpdownButtonProps = (pathname = "") => {
+  const pageId = pathname.split("/").pop();
+  if (pathname.indexOf("/posts/") !== -1) {
+    return {
+      id: pageId,
+      namespace: "posts",
+    };
+  }
+  if (pathname.indexOf("/dev-tips/") !== -1) {
+    return {
+      id: pageId,
+      namespace: "dev-tips",
+    };
+  }
+  return {
+    id: pageId,
+    namespace: "page",
+  };
+};
 
 export default function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
+  const showUpdownButton = canShowUpdownButton(pathname);
+  const updownButtonProps = getShowUpdownButtonProps(pathname);
+
   return (
     <>
-    <Head>
-      <link
-        rel="preload"
-        href="/fonts/Inter-roman.latin.var.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
-    </Head>
-    <Component {...pageProps} />
+      <Provider apiKey="pt_486fd32c067690889ba35056819b7f">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Inter-roman.latin.var.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Component {...pageProps} />
+        {showUpdownButton && (
+          <div className="like-dislike-button-container">
+            <UpdownButton {...updownButtonProps} />
+          </div>
+        )}
+      </Provider>
     </>
-  )
+  );
 }
